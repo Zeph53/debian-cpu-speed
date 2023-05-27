47626eec17ffffa1a52a10f586a15c887c3c0308
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2023-05-27 14:21:12 -0400

    Add files via upload

--- cpu-speed.bash
+++ cpu-speed.bash
@@ -8,8 +8,48 @@ CPUMAX="/sys/devices/system/cpu/cpufreq/policy*/cpuinfo_max_freq"
 CSCALE="/sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq"
 GOVERN="/sys/devices/system/cpu/cpufreq/policy*/scaling_available_governors"
 CURGOV="/sys/devices/system/cpu/cpufreq/policy*/scaling_governor"
-
-read -e -p "$(printf "Set the maximum CPU frequency in MHz ($(cat $CPUMIN | awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' | sed 's/...$//g')-$(cat $CPUMAX | awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' | sed 's/...$//g')) - Currently \"$(cat $CSCALE | awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' | sed 's/...$//g')\": ")" SETMAXSCALE
-printf "$SETMAXSCALE" | sed 's/$/000/g' | tee $CSCALE > /dev/null
-read -e -p "$(printf "Set the CPU governor mode ($(cat $GOVERN | sed 's/.$//g' | sed 's/ /, /g' | sort -u | sed 's/schedutil/default/g' | sed 's/powersav/powersave/g')) - Currently \"$(cat $CURGOV | sort -u | sed 's/schedutil/default/g')\": ")" GOVSEL
-printf "$GOVSEL" | sed 's/default/schedutil/g' | tee $CURGOV > /dev/null
+#
+## READ for user input with a prompt.
+read -e -p "$(\
+  printf "Set the maximum CPU frequency in MHz ($(\
+#
+## Display the minimum operating speed of the sum of all CPU cores.
+    cat $CPUMIN |\
+      awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
+      sed 's/...$//g')-$(\
+#
+## Display the maximum operating speed of the sum of all CPU cores.
+    cat $CPUMAX |\
+      awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
+      sed 's/...$//g' )) - Currently \"$(\
+#
+## Display the maximum CPU operating speed scaling factor.
+    cat $CSCALE |\
+      awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
+      sed 's/...$//g')\": ")"\
+  SETMAXSCALE
+#
+## Print the result of READ into each CPU's scaling factor for each available CPU.
+printf "$SETMAXSCALE" |\
+  sed 's/$/000/g' |\
+  tee $CSCALE >\
+  /dev/null
+#
+## Prompts the user to choose a governor using READ and scans for all governor modes.
+read -e -p "$(\
+  printf "Set the CPU governor mode ($(\
+    cat $GOVERN |\
+      sort -u \
+  )) - Currently \"$(\
+    cat $CURGOV |\
+      sort -u |\
+      sed 's/schedutil/default/g')\": ")"\
+  GOVSEL
+#
+## Print the result of READ into each CPU's scaling governor mode for each available CPU.
+printf "$GOVSEL" |\
+  sed 's/default/schedutil/g' |\
+  tee $CURGOV >\
+  /dev/null
+#
+## DONE
