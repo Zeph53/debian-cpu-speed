2bc06a71caf1d6951762c30d2b7887235f694472
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2023-09-07 15:47:58 -0400

    Add files via upload

--- cpu-speed.bash
+++ cpu-speed.bash
@@ -2,12 +2,16 @@
 
 #
 ## A simple script to set CPU speed 
-
+#
+## Environment variables.
 CPUMIN="/sys/devices/system/cpu/cpufreq/policy*/cpuinfo_min_freq"
 CPUMAX="/sys/devices/system/cpu/cpufreq/policy*/cpuinfo_max_freq"
-CSCALE="/sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq"
+CSCALEMIN="/sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq"
+CSCALEMAX="/sys/devices/system/cpu/cpufreq/policy*/scaling_max_freq"
 GOVERN="/sys/devices/system/cpu/cpufreq/policy*/scaling_available_governors"
 CURGOV="/sys/devices/system/cpu/cpufreq/policy*/scaling_governor"
+
+
 #
 ## READ for user input with a prompt.
 read -e -p "$(\
@@ -24,7 +28,7 @@ read -e -p "$(\
       sed 's/...$//g' )) - Currently \"$(\
 #
 ## Display the maximum CPU operating speed scaling factor.
-    cat $CSCALE |\
+    cat $CSCALEMAX |\
       awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
       sed 's/...$//g')\": ")"\
   SETMAXSCALE
@@ -32,8 +36,38 @@ read -e -p "$(\
 ## Print the result of READ into each CPU's scaling factor for each available CPU.
 printf "$SETMAXSCALE" |\
   sed 's/$/000/g' |\
-  tee $CSCALE >\
+  tee $CSCALEMAX >\
   /dev/null
+
+
+#
+## READ for user input with a prompt.
+read -e -p "$(\
+  printf "Set the minimum CPU frequency in MHz ($(\
+#
+## Display the minimum operating speed of the sum of all CPU cores.
+    cat $CPUMIN |\
+      awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
+      sed 's/...$//g')-$(\
+#
+## Display the maximum operating speed of the sum of all CPU cores.
+    cat $CPUMAX |\
+      awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
+      sed 's/...$//g' )) - Currently \"$(\
+#
+## Display the minimum CPU operating speed scaling factor.
+    cat $CSCALEMIN |\
+      awk '{for(i=1;i<=NF;i++){sum+=$i;num++}}END{print(sum/num)}' |\
+      sed 's/...$//g')\": ")"\
+  SETMINSCALE
+#
+## Print the result of READ into each CPU's scaling factor for each available CPU.
+printf "$SETMINSCALE" |\
+  sed 's/$/000/g' |\
+  tee $CSCALEMIN >\
+  /dev/null
+
+
 #
 ## Prompts the user to choose a governor using READ and scans for all governor modes.
 read -e -p "$(\
