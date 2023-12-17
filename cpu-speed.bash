0378addbbc2a28379865e4d51edef94a956eadeb
Zeph53 <102870900+Zeph53@users.noreply.github.com>
2023-12-17 01:05:34 -0500

    Add files via upload

--- cpu-speed.bash
+++ cpu-speed.bash
@@ -12,18 +12,34 @@ GOVERN="/sys/devices/system/cpu/cpufreq/policy*/scaling_available_governors"
 CURGOV="/sys/devices/system/cpu/cpufreq/policy*/scaling_governor"
 
 
-if [ $# -eq 2 ]; then
+if [ $# -eq 1 ] && { [ "$1" = "--help" ] || [ "$1" = "-h" ] || [ "$1" = "-?" ] || [ "$1" = "--?" ]; }; then
+    # Display usage instructions
+    printf "Usage: cpu-speed.bash <min_speed> <max_speed> <governor_mode>\n"
+    printf "Run the script without any arguments to use interactive mode.\n"
+    exit 0
+fi
+
+
+if [ $# -eq 3 ]; then
     ARGMIN=$1
     ARGMAX=$2
-    printf "$ARGMIN" |\
-      sed 's/$/000/g' |\
-      tee $CSCALEMIN >\
-      /dev/null
-    printf "$ARGMAX" |\
-      sed 's/$/000/g' |\
-      tee $CSCALEMAX >\
-      /dev/null
-    printf "Set CPU speed to $(cat $CSCALEMIN | sort -u | sed 's/...$//g')MHz - $(cat $CSCALEMAX | sort -u | sed 's/...$//g')MHz \n"
+    GOVSEL=$3
+    # Get available governor modes
+    AVAILABLE_GOVERNORS=($(cat $GOVERN | sort -u))
+    # Check if provided governor mode exists
+    if [[ " ${AVAILABLE_GOVERNORS[*]} " =~ " $GOVSEL " ]]; then
+        # Set CPU speeds
+        printf "$ARGMIN" | sed 's/$/000/g' | tee $CSCALEMIN >/dev/null
+        printf "$ARGMAX" | sed 's/$/000/g' | tee $CSCALEMAX >/dev/null
+        # Set CPU governor mode
+        printf "$GOVSEL" | sed 's/default/schedutil/g' | tee $CURGOV >/dev/null
+        # Display updated settings
+        printf "Set CPU speed to $(cat $CSCALEMIN | sort -u | sed 's/...$//g')MHz - $(cat $CSCALEMAX | sort -u | sed 's/...$//g')MHz\n"
+        printf "CPU governor mode set to $GOVSEL\n"
+    else
+        # If governor mode doesn't exist, display an error message
+        printf "Error: '$GOVSEL' is not a valid governor mode. Available modes: ${AVAILABLE_GOVERNORS[*]}\n"
+    fi
 else
 
 
@@ -83,30 +99,25 @@ else
     /dev/null
 
 
-#
-## Prompts the user to choose a governor using READ and scans for all governor modes.
-#  read -e -p "$(\
-#    printf "Set the CPU governor mode ($(\
-#      cat $GOVERN |\
-#        sort -u )) - Currently \"$(\
-#      cat $CURGOV |\
-#        sort -u |\
-#        sed 's/schedutil/default/g')\": ")"\
-#    GOVSEL
-
-
-#
-## Present numbered options for governor modes using select
-LISTGOVERNS=($(cat $GOVERN | sort -u))
-printf "Set the CPU governor mode:\n"
-select mode_option in "${LISTGOVERNS[@]}"; do
-    if [ -n "$mode_option" ]; then
-        GOVSEL="$mode_option"
-        break
-    else
-        echo "Invalid option. Please select a valid number."
+# Get the available governor modes
+AVAILABLE_GOVERNORS=($(cat $GOVERN | sort -u))
+# Create a string of numbered governor modes
+numbered_modes=""
+for ((i = 0; i < ${#AVAILABLE_GOVERNORS[@]}; i++)); do
+    numbered_modes+="$(($i + 1)).${AVAILABLE_GOVERNORS[i]}"
+    if [[ $i -ne $(( ${#AVAILABLE_GOVERNORS[@]} - 1 )) ]]; then
+        numbered_modes+=" "
     fi
 done
+# Display the available governor modes with numbers
+printf "Set the CPU governor mode ($numbered_modes) - Currently '$(cat $CURGOV | sort -u | sed 's/schedutil/default/g')': "
+read -r selected_mode_number
+# Validate the user input
+if [[ $selected_mode_number =~ ^[0-9]+$ && $selected_mode_number -ge 1 && $selected_mode_number -le ${#AVAILABLE_GOVERNORS[@]} ]]; then
+    GOVSEL="${AVAILABLE_GOVERNORS[selected_mode_number - 1]}"
+    printf "$GOVSEL" | sed 's/default/schedutil/g' | tee $CURGOV >/dev/null
+    printf "CPU governor mode set to $GOVSEL\n"
+fi
 
 
 #
